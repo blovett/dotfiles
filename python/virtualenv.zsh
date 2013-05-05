6c4ffe1f8c4aa87498dd6f2c7231bfe2cfaec10d
Ben Lovett <ben@tilderoot.com>
2013-05-05 12:06:36 -0700

    Reorganize my dotfiles based on @holman's dotfiles.

---
+++ python/virtualenv.zsh
@@ -0,0 +1,5 @@
+# python virtualenv
+export WORKON_HOME=$HOME/VirtualEnvs
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
---
+++ zsh/aliases.zsh
@@ -0,0 +1,6 @@
+alias reload!='. ~/.zshrc'
+
+# Global aliases (don't need to be at the beginning of the command line)
+alias -g L='|less'
+alias -g H='|head'
+alias -g T='|tail'
