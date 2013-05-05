e77c6867ff73fdd4fad92eec5b07346dc1e0e654
Ben Lovett <ben@tilderoot.com>
2013-05-05 15:01:35 -0700

    much prefer this prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -79,7 +79,8 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+#export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT="%m %~ %# "
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
