33c1ca88c2307c33b3ad9f35c1b5734dc71557be
Ben Lovett <ben@tilderoot.com>
2013-05-11 12:54:05 -0700

    modified path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,1 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="$ZSH/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
